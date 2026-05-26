1f79263be1945b99f256fb75fe0e868f35dd6a51
nageshwaribolla <bollanageshwari5@gmail.com>
2026-05-26 10:12:48 +0530

    day8 folder name Summer training tech1\JavaDSA

---
+++ src/day8/ArrayList1.java
@@ -0,0 +1,17 @@
+package day8;
+
+import java.util.ArrayList;
+
+public class ArrayList1 {
+     public static void main(String[] args) {
+        ArrayList<Integer> arr=new ArrayList<>();
+        arr.add(5);
+        arr.add(8);
+        arr.add(9);
+        arr.add(11);
+        System.out.println(arr);
+
+    }
+    
+    
+}
